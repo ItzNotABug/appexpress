8e8f5316b8a594e7ae09cb5381039a488e9edb62
ItzNotABug <itznotabug@gmail.com>
2024-10-04 23:56:51 +0530

    update: dependencies and api methods.

--- function/utils/middlewares.js
+++ function/utils/middlewares.js
@@ -5,14 +5,16 @@ import minifier from '@itznotabug/appexpress-minifier';
 import noCookies from '@itznotabug/appexpress-nocookies';
 import { authUserForConsoleMiddleware } from '../middlewares/auth.js';
 
+const cacheModule = apiCache({ timeout: 0 });
+
 export default (express) => {
     logEverything(express);
-    cacheEverything(express);
-    favIconMiddleware(express);
     minifierMiddleware(express);
-    express.middleware(cors.middleware);
-    express.middleware(noCookies.middleware);
+    express.middleware(cors());
+    express.middleware(noCookies());
+    express.middleware(cacheModule);
     express.middleware(authUserForConsoleMiddleware);
+    express.middleware(favIcon({ iconPath: 'icons/favicon.ico' }));
 };
 
 const logEverything = (express) => {
@@ -21,36 +23,23 @@ const logEverything = (express) => {
 
         // these won't be marked unsupported!
         console.log(`Requested Path: ${url}`);
-        if (apiCache.hasCache(url)) {
+        if (cacheModule.hasCache(url)) {
             console.log(`This url (${url}) is cached!`);
         }
     });
 };
 
-const cacheEverything = (express) => {
-    apiCache.options({ timeout: 0 });
-    express.middleware(apiCache.middleware);
-};
-
-const favIconMiddleware = (express) => {
-    favIcon.options({
-        iconPath: 'icons/favicon.ico',
-    });
-
-    express.middleware(favIcon.middleware);
-};
-
 const minifierMiddleware = (express) => {
-    minifier.options({
-        excludes: ['/robots.txt'],
-        htmlOptions: {
-            minifyJS: true,
-            minifyCSS: true,
-            removeComments: true,
-            collapseWhitespace: true,
-            preserveLineBreaks: false,
-        },
-    });
-
-    express.middleware(minifier.middleware);
+    express.middleware(
+        minifier({
+            excludes: ['/robots.txt'],
+            htmlOptions: {
+                minifyJS: true,
+                minifyCSS: true,
+                removeComments: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: false,
+            },
+        }),
+    );
 };
