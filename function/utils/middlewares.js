6a115c282e8e4e7ccdd1a12a0228c1384fdfaab5
ItzNotABug <itznotabug@gmail.com>
2024-10-05 12:09:03 +0530

    update: dependencies and api methods.

--- function/utils/middlewares.js
+++ function/utils/middlewares.js
@@ -1,19 +1,17 @@
 import cors from '@itznotabug/appexpress-cors';
 import favIcon from '@itznotabug/appexpress-favicon';
-import apiCache from '@itznotabug/appexpress-apicache';
 import minifier from '@itznotabug/appexpress-minifier';
+import * as cache from '@itznotabug/appexpress-apicache';
 import noCookies from '@itznotabug/appexpress-nocookies';
 import { authUserForConsoleMiddleware } from '../middlewares/auth.js';
-
-const cacheModule = apiCache({ timeout: 0 });
 
 export default (express) => {
     logEverything(express);
     minifierMiddleware(express);
     express.middleware(cors());
     express.middleware(noCookies());
-    express.middleware(cacheModule);
     express.middleware(authUserForConsoleMiddleware);
+    express.middleware(cache.createApiCache({ timeout: 0 }));
     express.middleware(favIcon({ iconPath: 'icons/favicon.ico' }));
 };
 
@@ -23,7 +21,7 @@ const logEverything = (express) => {
 
         // these won't be marked unsupported!
         console.log(`Requested Path: ${url}`);
-        if (cacheModule.hasCache(url)) {
+        if (cache.hasCache(url)) {
             console.log(`This url (${url}) is cached!`);
         }
     });
